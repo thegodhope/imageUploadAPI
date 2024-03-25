f13bf982752902fcffbcf789a50370e8059eaf7e
thegodhope <103210115+thegodhope@users.noreply.github.com>
2024-03-25 21:42:46 +0100

    controllers

---
+++ controller/uploadController.js
@@ -0,0 +1,18 @@
+const cloudinary = require('../utils/cloudinary');
+
+exports.uploadImage = (req, res) => {
+    cloudinary.uploader.upload(req.file.path, function(err, result) {
+        if (err) {
+            console.log(err);
+            return res.status(500).json({
+                success: false,
+                message: "Error"
+            });
+        }
+        res.status(200).json({
+            success: true,
+            message: "Uploaded!",
+            data: result
+        });
+    });
+};
